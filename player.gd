300a609f514f21a46dbc1b9ebd5b0d8e1ceaa2f5
Daniel <106932885+daniels-4@users.noreply.github.com>
2023-12-13 22:41:53 -0500

    more character movement
    added quick falling, A and D movement
    modularized basic player script some

--- player.gd
+++ player.gd
@@ -3,20 +3,16 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
+@export var JUMP_RELEASE_PEAK: int = -150.0
+@export var JUMP_RETURN_GRAVITY: int = 5.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	apply_gravity(delta)
+	jump_handler()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -26,3 +22,17 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func apply_gravity(frame):
+	if not is_on_floor():
+		velocity.y += gravity * frame
+		
+func jump_handler():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_RELEASE_PEAK:
+			velocity.y = JUMP_RELEASE_PEAK
+		if velocity.y > 0:
+			velocity.y += JUMP_RETURN_GRAVITY
