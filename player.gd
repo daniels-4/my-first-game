951af40a44f07789cc7195021946368a1ffa2c9a
Daniel <106932885+daniels-4@users.noreply.github.com>
2023-12-14 19:20:45 -0500

    more animations and char skins
    added 2 more sprite anims
    player faces right direction when moving
    added tan player skin

--- player.gd
+++ player.gd
@@ -6,10 +6,14 @@ const JUMP_VELOCITY = -300.0
 @export var JUMP_RELEASE_PEAK: int = -150.0
 @export var JUMP_RETURN_GRAVITY: int = 5.0
 
+@onready var animatedSprite = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
+func _ready():
+	animatedSprite.frames = load("res://PlayerSandSkin.tres")
+	
 func _physics_process(delta):
 	apply_gravity(delta)
 	jump_handler()
@@ -18,8 +22,16 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animatedSprite.animation = "run"
+		if direction > 0:
+			player_face_flip(true)
+		if direction < 0:
+			player_face_flip(false)
+		else:
+			pass
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animatedSprite.animation = "idle"
 
 	move_and_slide()
 	
@@ -32,7 +44,11 @@ func jump_handler():
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
 	else:
+		animatedSprite.animation = "jump"
 		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_RELEASE_PEAK:
 			velocity.y = JUMP_RELEASE_PEAK
 		if velocity.y > 0:
 			velocity.y += JUMP_RETURN_GRAVITY
+			
+func player_face_flip(flip):
+	animatedSprite.flip_h = flip
